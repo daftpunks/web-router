26f81d88e73522326e76cac528a8ba6afe49ff33
derekalangilbert <derekgilbert@outlook.com>
2020-05-20 22:41:41 -0600

    added some things

--- index.js
+++ index.js
@@ -1,57 +1,32 @@
 // env
 require('dotenv').config()
 // express
-const path = require('path');
-const express = require('express');
-const proxy = require("express-http-proxy");
-const app = express();
-// koa
-const koa = require('koa')
-const router = require('koa-router')
-const serve = ('koa-static')
+const path = require('path')
+const express = require('express')
+const app = express()
+
 // makes distribution folder servable
-app.use(express.static(__dirname + "/distribution/"));
-app.use(express.static(__dirname + "/clients/"));
+app.use(express.static(__dirname + '/distribution/'))
+app.use(express.static(__dirname + '/clients/'))
 
 // routes everything after /s to seach app and client side routing takes over.
 
 app.get('/s/*', (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname, '/distribution/search/index.html'));
-});
+  res.setHeader('Content-Type', 'text/html')
+  res.sendFile(path.join(__dirname, '/distribution/search/index.html'))
+})
 
 app.get('/s/*', (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname, '/distribution/search/index.html'));
-});
-  app.get('/a/*', (req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.sendFile(path.join(__dirname, '/distribution/account/index.html'))
-  })
+  res.setHeader('Content-Type', 'text/html')
+  res.sendFile(path.join(__dirname, '/distribution/search/index.html'))
+})
+app.get('/a/*', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.sendFile(path.join(__dirname, '/distribution/account/index.html'))
+})
 
-  // listing resolver
-  app.use("/l/home-for-sale/*/:id", proxy("localhost:44320", {
-      proxyReqPathResolver: function(req) {
-        const pathArray = req.baseUrl.split('/')
-        const id = req.params.id
-        const newPath = req.path + pathArray[2] + '/' + pathArray[3] + '/' + id
-        return newPath
-      }
-    })
-  );
- // corporate resolver
-    app.use('/c/*', proxy('https://www-homie-dev.azurewebsites.net/', {
-        proxyReqPathResolver: function(req) {
-          const pathArray = req.baseUrl.split('/')
-          const newPath = req.path + pathArray[2]
-          return newPath
-        }
-      })
-    )
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/distribution/static/404.html'))
+})
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/distribution/static/404.html"));
-});
-
-
-app.listen(3000, () => console.log('server running'));
+app.listen(3000, () => console.log('server running .. hi ✋'))
